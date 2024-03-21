a0dbc51925416a320badbe14f22f5f46756f64b4
gracee <124023610+gracedeepa@users.noreply.github.com>
2024-03-22 06:54:44 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-3f0a0dec-c965-4d7b-86e0-17b8bbed0aa4/)
 
 
 echo "$curl_output" >> Update.tsx
